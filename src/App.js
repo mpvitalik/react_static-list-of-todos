4dfc10dc6cf23e0020a2e77ec96ce09e5d2acfc1
mpvitalik <mpvitalik@gmail.com>
2019-07-03 21:46:38 +0300

    Solution

--- src/App.js
+++ src/App.js
@@ -4,21 +4,46 @@ import './App.css';
 import todos from './api/todos';
 import users from './api/users';
 
+function getUser(userId) {
+  return users.find(user => user.id === userId);
+}
+
+const todosWithUser = todos.map((todo) => {
+  return {
+    ...todo,
+    user: getUser(todo.userId),
+  };
+});
+
 function App() {
   return (
     <div className="App">
       <h1>Static list of todos</h1>
-      <p>
-        <span>Todos: </span>
-        {todos.length}
-      </p>
 
-      <p>
-        <span>Users: </span>
-        {users.length}
-      </p>
+      <TodoList todos = {todosWithUser} />
+
     </div>
   );
-}
+};
+
+const TodoList = (props) => (
+  <ul>
+    {props.todos.map(todo => (
+      <TodoItem todo = {todo}/>
+    ))}
+  </ul>
+);
+
+const TodoItem = (props) => (
+  <li>
+    <input type="checkbox" checked = {props.todo.completed} />
+    <div>{props.todo.title}</div>
+    <User user = {props.todo.user}/>
+  </li>
+);
+
+const User = (props) => (
+  <div>{props.user.name}</div>
+);
 
 export default App;
